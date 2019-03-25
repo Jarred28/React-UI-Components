7e18ae3b183356e7a7762094d972a94d08ea51df
jarred richards <jarred2828@gmail.com>
2019-03-25 18:42:11 -0400

    MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner'
+import CardContent from './CardContent'
+
+function CardContainer(){
+    return(
+        <a className="card-link" href="">
+        <div className="twitter-body">
+            <CardBanner />
+            <CardContent />
+        </div>
+        </a>
+    )
+}
